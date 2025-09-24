b158781f0dad45869b858bd4a5e9744ef83e5dea
Ezerike Caleb <cezerike@gmail.com>
2025-09-24 04:37:36 -0700

    'fix'

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    visualizer({ open: true, gzipSize: true, brotliSize: true }),
+  ],
   server: {
     port: 3000,
   },
